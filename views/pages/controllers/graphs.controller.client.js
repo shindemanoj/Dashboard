6326194dfb3720b16433b42c57321024b7214610
Manoj Shinde <shinde.m@husky.neu.edu>
2017-10-25 08:50:00 -0400

    Uptaded order of reports

--- views/pages/controllers/graphs.controller.client.js
+++ views/pages/controllers/graphs.controller.client.js
@@ -409,9 +409,9 @@
             function GetSortOrderReverse(prop) {
                 return function(a, b) {
                     if (a[prop] > b[prop]) {
-                        return 1;
-                    } else if (a[prop] < b[prop]) {
                         return -1;
+                    } else if (a[prop] < b[prop]) {
+                        return 1;
                     }
                     return 0;
                 }
